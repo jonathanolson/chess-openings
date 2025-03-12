9f08541e268f19dcfd691760c4172cd9f428e6f2
Jonathan Olson <jonathan.olson@colorado.edu>
2025-03-12 00:33:11 -0600

    notes

--- src/model/getStockfishWorker.ts
+++ src/model/getStockfishWorker.ts
@@ -1,6 +1,11 @@
 // @ts-expect-error Because worker
 import StockfishWorker from "stockfish/src/stockfish-nnue-16-single.js?worker";
 
+// COOP/COEP notes (for multi-threaded stockfish):
+// https://github.com/orgs/community/discussions/13309
+// https://github.com/GoogleChrome/workbox/issues/2963
+// https://www.captaincodeman.com/cross-origin-isolation-with-sveltekit-vite-and-firebase
+
 let hintWorker: Worker | null = null;
 
 export const getStockfishWorker = (): Worker => {
