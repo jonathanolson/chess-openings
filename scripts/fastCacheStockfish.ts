97e648b67e13fb91efdfacdb47b31b34058f9644
Jonathan Olson <jonathan.olson@colorado.edu>
2025-03-14 13:37:42 -0600

    biased ordering for stockfish

--- scripts/fastCacheStockfish.ts
+++ scripts/fastCacheStockfish.ts
@@ -1,5 +1,5 @@
 import chessOpenings from "../src/data/chessOpenings.js";
-import { Nodes } from "../src/model/ChessNode.js";
+import { ChessNode, Nodes } from "../src/model/ChessNode.js";
 import { compactStateToNodes } from "../src/model/compactStateToNodes.js";
 import { scanConflicts } from "../src/model/scanConflicts.js";
 // @ts-expect-error Some options
@@ -8,7 +8,7 @@ import _ from "lodash";
 import fs from "fs";
 // @ts-expect-error Some options
 import os from "os";
-import { Fen } from "../src/model/common.js";
+import { Fen, Move } from "../src/model/common.js";
 // @ts-expect-error Some options
 import child_process from "child_process";
 import { Chess } from "chess.js";
@@ -26,7 +26,12 @@ const depth = 36;
 
 const orderMethod = process.argv[2];
 
-if (orderMethod !== "depth" && orderMethod !== "popularity") {
+if (
+  orderMethod !== "depth" &&
+  orderMethod !== "popularity" &&
+  orderMethod !== "biased" &&
+  orderMethod !== "biasedLeaf"
+) {
   throw new Error(`Invalid order method: ${orderMethod}`);
 }
 
@@ -56,6 +61,15 @@ os.setPriority(os.constants.priority.PRIORITY_LOW);
 
   let fens: Fen[];
 
+  type PopularityEntry = {
+    fen: Fen;
+    popularity: number;
+  };
+
+  const mainExplore: CompactLichessExplore = JSON.parse(
+    fs.readFileSync("./src/data/lichessExploreBlitzLowDeep.json", "utf8"),
+  );
+
   if (orderMethod === "depth") {
     const whiteFens = Object.keys(whiteNodes);
     const blackFens = Object.keys(blackNodes);
@@ -75,15 +89,6 @@ os.setPriority(os.constants.priority.PRIORITY_LOW);
       ...fenExtend(blackExtendedFens),
     ]);
   } else if (orderMethod === "popularity") {
-    const mainExplore: CompactLichessExplore = JSON.parse(
-      fs.readFileSync("./src/data/lichessExploreBlitzLowDeep.json", "utf8"),
-    );
-
-    type PopularityEntry = {
-      fen: Fen;
-      popularity: number;
-    };
-
     console.log("computing popularity map");
 
     const popularityMap: Record<Fen, number> = {};
@@ -120,6 +125,240 @@ os.setPriority(os.constants.priority.PRIORITY_LOW);
     fens = _.sortBy(popularityEntries, [(entry) => -entry.popularity]).map(
       (entry) => entry.fen,
     );
+  } else if (orderMethod === "biased" || orderMethod === "biasedLeaf") {
+    const leafOnly = orderMethod === "biasedLeaf";
+
+    const skipZeroProbability = true;
+
+    console.log("computing popularity map");
+
+    const popularityMap: Record<
+      Fen,
+      { popularity: number; histories: Move[][] }
+    > = {};
+
+    const getExploreMoveCount = (explore: CompactLichessExplore, move: Move) =>
+      explore.m && explore.m[move]
+        ? explore.m[move].d[0] + explore.m[move].d[1] + explore.m[move].d[2]
+        : 0;
+
+    const runNodes = (nodes: Nodes, isWhite: boolean) => {
+      const breadthFirstChessNodes: ChessNode[] = [];
+      const historyMap = new Map<ChessNode, Move[]>();
+
+      {
+        const rootNode = nodes[initialFen];
+        const visited = new Set<ChessNode>();
+        const queue: Array<[ChessNode, Move[]]> = [];
+
+        queue.push([rootNode, []]);
+        visited.add(rootNode);
+
+        while (queue.length > 0) {
+          const [currentNode, currentHistory] = queue.shift()!;
+
+          breadthFirstChessNodes.push(currentNode);
+          historyMap.set(currentNode, currentHistory);
+
+          // console.log(isWhite ? "white" : "black", currentHistory.join(" "));
+
+          for (const move of currentNode.moves) {
+            const nextNode = currentNode.moveMap[move];
+            if (!visited.has(nextNode)) {
+              visited.add(nextNode);
+              queue.push([nextNode, [...currentHistory, move]]);
+            }
+          }
+        }
+      }
+
+      for (const chessNode of breadthFirstChessNodes) {
+        // Skip non-leaves if we are only looking at leaves
+        if (leafOnly && chessNode.children.length > 0) {
+          continue;
+        }
+
+        let baseProbability = 0;
+        let explores: CompactLichessExplore[] = [];
+
+        const histories = chessNode.getHistories();
+
+        for (const history of histories) {
+          let explore: CompactLichessExplore | null = mainExplore;
+          let board = new Chess();
+          let probability = 1;
+
+          for (let i = 0; i < history.length; i++) {
+            const move = history[i];
+
+            let possibleMoveCount = board.moves().length;
+            let moveProbability =
+              possibleMoveCount > 0 ? 1 / possibleMoveCount : 0;
+            if (explore && explore.m) {
+              const getMoveCount = (move: Move) =>
+                explore.m[move].d[0] +
+                explore.m[move].d[1] +
+                explore.m[move].d[2];
+
+              const totalMoveCount = Object.keys(explore.m).reduce(
+                (sum, move) => sum + getMoveCount(move),
+                0,
+              );
+              if (totalMoveCount > 0) {
+                moveProbability = getMoveCount(move) / totalMoveCount;
+              }
+            }
+
+            // Only count probability for OPPONENT moves (at least when getting history of the chess node
+            if (isWhite !== (board.turn() === "w")) {
+              probability *= moveProbability;
+            }
+
+            board.move(move);
+            explore = explore?.m[move] ?? null;
+          }
+
+          baseProbability += probability;
+          if (explore) {
+            explores.push(explore);
+          }
+        }
+
+        console.log(isWhite ? "white" : "black", histories[0], baseProbability);
+
+        const addPopularity = (fen: Fen, popularity: number) => {
+          if (popularity === 0 && skipZeroProbability) {
+            return;
+          }
+
+          if (popularityMap[fen]) {
+            popularityMap[fen].popularity += popularity;
+          } else {
+            popularityMap[fen] = {
+              popularity,
+              histories: histories,
+            };
+          }
+        };
+
+        addPopularity(chessNode.fen, baseProbability);
+
+        if (baseProbability === 0 && skipZeroProbability) {
+          continue;
+        }
+
+        let totalNextCount = 0;
+        for (const explore of explores) {
+          if (explore.m) {
+            for (const move of Object.keys(explore.m)) {
+              totalNextCount += getExploreMoveCount(explore, move);
+            }
+          }
+        }
+
+        const nextMoves = new Chess(chessNode.fen).moves();
+        for (const move of nextMoves) {
+          const nextBoard = new Chess(chessNode.fen);
+          nextBoard.move(move);
+          const nextFen = getFen(nextBoard);
+
+          // If it will be included directly, exclude this!
+          if (nodes[nextFen]) {
+            continue;
+          }
+
+          let partialNextProbability = 1 / nextMoves.length;
+
+          let nextExplores: CompactLichessExplore[] = [];
+
+          if (totalNextCount > 0) {
+            let moveCount = 0;
+            for (const explore of explores) {
+              if (explore && explore.m) {
+                moveCount += getExploreMoveCount(explore, move);
+                nextExplores.push(explore);
+              }
+            }
+            partialNextProbability = moveCount / totalNextCount;
+          }
+
+          const nextProbability = baseProbability * partialNextProbability;
+
+          // console.log(`  ${move}`, nextProbability);
+
+          addPopularity(nextFen, nextProbability);
+
+          if (nextProbability === 0 && skipZeroProbability) {
+            continue;
+          }
+
+          // If it is our move next, then we will evaluate our moves another level
+          if (isWhite === (nextBoard.turn() === "w")) {
+            let totalNextNextCount = 0;
+            for (const nextExplore of nextExplores) {
+              if (nextExplore.m) {
+                for (const nextMove of Object.keys(nextExplore.m)) {
+                  totalNextNextCount += getExploreMoveCount(
+                    nextExplore,
+                    nextMove,
+                  );
+                }
+              }
+            }
+
+            const nextNextMoves = nextBoard.moves();
+            for (const nextMove of nextNextMoves) {
+              const nextNextBoard = new Chess(nextFen);
+              nextNextBoard.move(nextMove);
+              const nextNextFen = getFen(nextNextBoard);
+
+              // If it will be included directly, exclude this!
+              if (nodes[nextNextFen]) {
+                continue;
+              }
+
+              let partialNextNextProbability = 1 / nextNextMoves.length;
+
+              if (totalNextNextCount > 0) {
+                let moveCount = 0;
+                for (const explore of nextExplores) {
+                  if (explore && explore.m) {
+                    moveCount += getExploreMoveCount(explore, nextMove);
+                  }
+                }
+                partialNextNextProbability = moveCount / totalNextNextCount;
+              }
+
+              const nextNextProbability =
+                nextProbability * partialNextNextProbability;
+
+              // console.log(`    ${nextMove}`, nextNextProbability);
+
+              addPopularity(nextNextFen, nextNextProbability);
+            }
+          }
+        }
+      }
+    };
+    runNodes(whiteNodes, true);
+    runNodes(blackNodes, false);
+
+    const popularityEntries: PopularityEntry[] = Object.keys(popularityMap).map(
+      (fen) => ({
+        fen,
+        popularity: popularityMap[fen].popularity,
+      }),
+    );
+
+    const orderedPopularityEntries = _.sortBy(popularityEntries, [
+      (entry) => -entry.popularity,
+    ]);
+
+    // console.log(orderedPopularityEntries.slice(10));
+    //
+    // throw new Error("STILL TESTING");
+
+    fens = orderedPopularityEntries.map((entry) => entry.fen);
   } else {
     throw new Error(`Missing order method implementation: ${orderMethod}`);
   }
