0c95d158098ac31817279f662816e109589b15f0
Jonathan Olson <jonathan.olson@colorado.edu>
2025-03-16 13:02:07 -0600

    hooks comment out stockfish for smaller build

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,13 +1,13 @@
-import { Stockfish, StockfishResult } from "./model/Stockfish.js";
-
-const stockfish = new Stockfish({
-  hashSizeInMegabytes: 128,
-});
-
-// @ts-expect-error global defined
-window.getEvaluation = async (
-  fen: string,
-  timeString: string,
-): Promise<StockfishResult> => {
-  return stockfish.getEvaluation(fen, timeString);
-};
+// import { Stockfish, StockfishResult } from "./model/Stockfish.js";
+//
+// const stockfish = new Stockfish({
+//   hashSizeInMegabytes: 128,
+// });
+//
+// // @ts-expect-error global defined
+// window.getEvaluation = async (
+//   fen: string,
+//   timeString: string,
+// ): Promise<StockfishResult> => {
+//   return stockfish.getEvaluation(fen, timeString);
+// };
