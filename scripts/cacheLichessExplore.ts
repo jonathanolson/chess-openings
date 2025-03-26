5374857582b4987978b4ee26ab64a77d12b30c52
Jonathan Olson <jonathan.olson@colorado.edu>
2025-03-26 17:35:17 -0600

    error handling on lichess explore

--- scripts/cacheLichessExplore.ts
+++ scripts/cacheLichessExplore.ts
@@ -25,254 +25,175 @@ const ONLY_BLITZ_LOW = false;
 const ONLY_UNDER_HISTORY: Move[] = [];
 
 (async () => {
-  const whiteNodes: Nodes = {};
-  const blackNodes: Nodes = {};
+  const main = async () => {
+    const whiteNodes: Nodes = {};
+    const blackNodes: Nodes = {};
 
-  compactStateToNodes(whiteNodes, chessOpenings.white, true);
-  compactStateToNodes(blackNodes, chessOpenings.black, false);
+    compactStateToNodes(whiteNodes, chessOpenings.white, true);
+    compactStateToNodes(blackNodes, chessOpenings.black, false);
 
-  scanConflicts(whiteNodes, true);
-  scanConflicts(blackNodes, false);
+    scanConflicts(whiteNodes, true);
+    scanConflicts(blackNodes, false);
 
-  const scan = async (
-    filename: string,
-    type: LichessExploreType,
-    includeExpansion: boolean,
-  ) => {
-    if (ONLY_BLITZ_LOW && type !== "blitzLow") {
-      return;
-    }
+    const scan = async (
+      filename: string,
+      type: LichessExploreType,
+      includeExpansion: boolean,
+    ) => {
+      if (ONLY_BLITZ_LOW && type !== "blitzLow") {
+        return;
+      }
 
-    const mainExplore: CompactLichessExplore = JSON.parse(
-      fs.readFileSync(`./src/data/${filename}.json`, "utf8"),
-    );
+      const mainExplore: CompactLichessExplore = JSON.parse(
+        fs.readFileSync(`./src/data/${filename}.json`, "utf8"),
+      );
 
-    // console.log("optimizing");
-    // {
-    //   const recur = (explore: CompactLichessExplore, keys: Move[]) => {
-    //     if (explore.m) {
-    //       const moves = Object.keys(explore.m);
-    //
-    //       for (const move of moves) {
-    //         const subExplore = explore.m[move];
-    //
-    //         if (
-    //           !subExplore.m &&
-    //           subExplore.d[0] === 0 &&
-    //           subExplore.d[1] === 0 &&
-    //           subExplore.d[2] === 0
-    //         ) {
-    //           console.log(`removing ${keys.join(" ")} ${move}`);
-    //           delete explore.m[move];
-    //         } else {
-    //           recur(subExplore, [...keys, move]);
-    //         }
-    //       }
-    //     }
-    //   };
-    //   recur(mainExplore, []);
-    // }
-    // save();
+      // console.log("optimizing");
+      // {
+      //   const recur = (explore: CompactLichessExplore, keys: Move[]) => {
+      //     if (explore.m) {
+      //       const moves = Object.keys(explore.m);
+      //
+      //       for (const move of moves) {
+      //         const subExplore = explore.m[move];
+      //
+      //         if (
+      //           !subExplore.m &&
+      //           subExplore.d[0] === 0 &&
+      //           subExplore.d[1] === 0 &&
+      //           subExplore.d[2] === 0
+      //         ) {
+      //           console.log(`removing ${keys.join(" ")} ${move}`);
+      //           delete explore.m[move];
+      //         } else {
+      //           recur(subExplore, [...keys, move]);
+      //         }
+      //       }
+      //     }
+      //   };
+      //   recur(mainExplore, []);
+      // }
+      // save();
 
-    if (includeExpansion) {
-      type Candidate = {
-        history: Move[];
-        popularity: number;
-      };
-
-      const getCandidates = (): Candidate[] => {
-        const candidates: Candidate[] = [];
-
-        const recur = (explore: CompactLichessExplore, moves: Move[]) => {
-          if (explore.m) {
-            // NOTE: This may not be all legal moves!
-
-            for (const move of Object.keys(explore.m)) {
-              const subExplore = explore.m[move];
-
-              if (subExplore.d[0] + subExplore.d[1] + subExplore.d[2] > 0) {
-                recur(subExplore, [...moves, move]);
-              }
-            }
-          } else {
-            let moveExtension = Number.POSITIVE_INFINITY;
-            let popularityCompensation = 1;
-
-            const nodeScan = (nodes: Nodes, isWhite: boolean) => {
-              let chessNode = nodes[initialFen];
-              const existingMoves: Move[] = [];
-              let compensation = 1;
-              let scanExplore = mainExplore;
-
-              for (const move of moves) {
-                if (chessNode.moves.includes(move)) {
-                  existingMoves.push(move);
-                  chessNode = chessNode.moveMap[move];
-
-                  let nextScanExplore = scanExplore.m[move];
-
-                  // compensate for loss of "popularity" on our move
-                  if (moves.indexOf(move) % 2 === (isWhite ? 0 : 1)) {
-                    const basePopularity =
-                      scanExplore.d[0] + scanExplore.d[1] + scanExplore.d[2];
-                    const nextPopularity =
-                      nextScanExplore.d[0] +
-                      nextScanExplore.d[1] +
-                      nextScanExplore.d[2];
-
-                    compensation *= basePopularity / nextPopularity;
-                  }
-
-                  scanExplore = nextScanExplore;
-                } else {
-                  break;
-                }
-              }
-
-              moveExtension = Math.min(
-                moveExtension,
-                moves.length - existingMoves.length,
-              );
-            };
-            nodeScan(whiteNodes, true);
-            nodeScan(blackNodes, false);
-
-            // TODO: change behavior for whether we are "on color"? -- or not, in case we want to modify our moves hmm
-            if (moveExtension <= 1) {
-              let popularity =
-                (explore.d[0] + explore.d[1] + explore.d[2]) *
-                popularityCompensation;
-
-              for (const boostLine of boostLines) {
-                if (
-                  moves.length >= boostLine.length &&
-                  boostLine.every((move, i) => moves[i] === move)
-                ) {
-                  popularity *= 25;
-                }
-              }
-
-              candidates.push({
-                history: moves,
-                popularity: popularity,
-              });
-            }
-          }
+      if (includeExpansion) {
+        type Candidate = {
+          history: Move[];
+          popularity: number;
         };
 
-        recur(mainExplore, []);
+        const getCandidates = (): Candidate[] => {
+          const candidates: Candidate[] = [];
 
-        return _.sortBy(candidates, [(candidate) => -candidate.popularity]);
-      };
+          const recur = (explore: CompactLichessExplore, moves: Move[]) => {
+            if (explore.m) {
+              // NOTE: This may not be all legal moves!
 
-      while (true) {
-        const candidates = getCandidates();
-        console.log(`${candidates.length} remaining`);
-        const histories = candidates
-          .slice(0, 30)
-          .map((candidate) => candidate.history);
+              for (const move of Object.keys(explore.m)) {
+                const subExplore = explore.m[move];
 
-        if (!histories.length) {
-          break;
-        }
+                if (subExplore.d[0] + subExplore.d[1] + subExplore.d[2] > 0) {
+                  recur(subExplore, [...moves, move]);
+                }
+              }
+            } else {
+              let moveExtension = Number.POSITIVE_INFINITY;
+              let popularityCompensation = 1;
 
-        for (const history of histories) {
-          await sleep(5000);
+              const nodeScan = (nodes: Nodes, isWhite: boolean) => {
+                let chessNode = nodes[initialFen];
+                const existingMoves: Move[] = [];
+                let compensation = 1;
+                let scanExplore = mainExplore;
 
-          console.log(
-            `[${type}] ${includeExpansion ? "expand " : ""}${history.join(" ")}`,
-          );
+                for (const move of moves) {
+                  if (chessNode.moves.includes(move)) {
+                    existingMoves.push(move);
+                    chessNode = chessNode.moveMap[move];
 
-          let explore = mainExplore;
-          for (const move of history) {
-            explore = explore.m[move];
+                    let nextScanExplore = scanExplore.m[move];
+
+                    // compensate for loss of "popularity" on our move
+                    if (moves.indexOf(move) % 2 === (isWhite ? 0 : 1)) {
+                      const basePopularity =
+                        scanExplore.d[0] + scanExplore.d[1] + scanExplore.d[2];
+                      const nextPopularity =
+                        nextScanExplore.d[0] +
+                        nextScanExplore.d[1] +
+                        nextScanExplore.d[2];
+
+                      compensation *= basePopularity / nextPopularity;
+                    }
+
+                    scanExplore = nextScanExplore;
+                  } else {
+                    break;
+                  }
+                }
+
+                moveExtension = Math.min(
+                  moveExtension,
+                  moves.length - existingMoves.length,
+                );
+              };
+              nodeScan(whiteNodes, true);
+              nodeScan(blackNodes, false);
+
+              // TODO: change behavior for whether we are "on color"? -- or not, in case we want to modify our moves hmm
+              if (moveExtension <= 1) {
+                let popularity =
+                  (explore.d[0] + explore.d[1] + explore.d[2]) *
+                  popularityCompensation;
+
+                for (const boostLine of boostLines) {
+                  if (
+                    moves.length >= boostLine.length &&
+                    boostLine.every((move, i) => moves[i] === move)
+                  ) {
+                    popularity *= 25;
+                  }
+                }
+
+                candidates.push({
+                  history: moves,
+                  popularity: popularity,
+                });
+              }
+            }
+          };
+
+          recur(mainExplore, []);
+
+          return _.sortBy(candidates, [(candidate) => -candidate.popularity]);
+        };
+
+        while (true) {
+          const candidates = getCandidates();
+          console.log(`${candidates.length} remaining`);
+          const histories = candidates
+            .slice(0, 30)
+            .map((candidate) => candidate.history);
+
+          if (!histories.length) {
+            break;
           }
 
-          if (explore.m) {
-            throw new Error("should not have m");
-          }
-
-          const data = await getLichessExplore(history, type);
-
-          explore.m = {};
-
-          for (const exploreMove of data.moves) {
-            explore.m[exploreMove.san] = {
-              d: [exploreMove.white, exploreMove.draws, exploreMove.black],
-            };
-          }
-
-          fs.writeFileSync(
-            `./src/data/${filename}.json`,
-            JSON.stringify(mainExplore),
-          );
-        }
-      }
-    } else {
-      const histories: Move[][] = [[]];
-
-      while (histories.length) {
-        const history = histories.shift();
-
-        let skip = false;
-        for (
-          let i = 0;
-          i < history.length && i < ONLY_UNDER_HISTORY.length;
-          i++
-        ) {
-          if (ONLY_UNDER_HISTORY[i] !== history[i]) {
-            skip = true;
-          }
-        }
-        if (skip) {
-          continue;
-        }
-
-        const board = new Chess();
-        history.forEach((move) => {
-          board.move(move);
-        });
-
-        const fen = getFen(board);
-
-        const whiteNode = whiteNodes[fen];
-        const blackNode = blackNodes[fen];
-
-        let moves: Move[] = [];
-
-        if (whiteNode) {
-          moves.push(...whiteNode.moves);
-        }
-        if (blackNode) {
-          moves.push(...blackNode.moves);
-        }
-        moves = _.uniq(moves);
-        if (includeExpansion && (whiteNode || blackNode)) {
-          // Include all moves if we have a node and we are including expansions
-          moves = board.moves();
-        }
-
-        // Set up the future
-        for (const move of moves) {
-          histories.push([...history, move]);
-        }
-
-        let explore = mainExplore;
-
-        // console.log(`checking ${history.join(" ")}`);
-
-        const appliedMoves: Move[] = [];
-        const appliedBoard = new Chess();
-
-        const tryFetch = async () => {
-          if (!explore.m) {
+          for (const history of histories) {
             await sleep(5000);
 
             console.log(
-              `[${type}] ${includeExpansion ? "expand " : ""}${appliedMoves.join(" ")}`,
+              `[${type}] ${includeExpansion ? "expand " : ""}${history.join(" ")}`,
             );
 
-            const data = await getLichessExplore(appliedMoves, type);
+            let explore = mainExplore;
+            for (const move of history) {
+              explore = explore.m[move];
+            }
+
+            if (explore.m) {
+              throw new Error("should not have m");
+            }
+
+            const data = await getLichessExplore(history, type);
 
             explore.m = {};
 
@@ -287,65 +208,161 @@ const ONLY_UNDER_HISTORY: Move[] = [];
               JSON.stringify(mainExplore),
             );
           }
-        };
+        }
+      } else {
+        const histories: Move[][] = [[]];
 
-        await tryFetch();
+        while (histories.length) {
+          const history = histories.shift();
 
-        for (const move of history) {
-          // Fill in missing spots lazily (as blanks)
-          if (!explore.m[move]) {
-            explore.m[move] = {
-              d: [0, 0, 0],
-            };
+          let skip = false;
+          for (
+            let i = 0;
+            i < history.length && i < ONLY_UNDER_HISTORY.length;
+            i++
+          ) {
+            if (ONLY_UNDER_HISTORY[i] !== history[i]) {
+              skip = true;
+            }
+          }
+          if (skip) {
+            continue;
           }
 
-          explore = explore.m[move];
-          appliedMoves.push(move);
-          appliedBoard.move(move);
+          const board = new Chess();
+          history.forEach((move) => {
+            board.move(move);
+          });
+
+          const fen = getFen(board);
+
+          const whiteNode = whiteNodes[fen];
+          const blackNode = blackNodes[fen];
+
+          let moves: Move[] = [];
+
+          if (whiteNode) {
+            moves.push(...whiteNode.moves);
+          }
+          if (blackNode) {
+            moves.push(...blackNode.moves);
+          }
+          moves = _.uniq(moves);
+          if (includeExpansion && (whiteNode || blackNode)) {
+            // Include all moves if we have a node and we are including expansions
+            moves = board.moves();
+          }
+
+          // Set up the future
+          for (const move of moves) {
+            histories.push([...history, move]);
+          }
+
+          let explore = mainExplore;
+
+          // console.log(`checking ${history.join(" ")}`);
+
+          const appliedMoves: Move[] = [];
+          const appliedBoard = new Chess();
+
+          const tryFetch = async () => {
+            if (!explore.m) {
+              await sleep(5000);
+
+              console.log(
+                `[${type}] ${includeExpansion ? "expand " : ""}${appliedMoves.join(" ")}`,
+              );
+
+              const data = await getLichessExplore(appliedMoves, type);
+
+              explore.m = {};
+
+              for (const exploreMove of data.moves) {
+                explore.m[exploreMove.san] = {
+                  d: [exploreMove.white, exploreMove.draws, exploreMove.black],
+                };
+              }
+
+              fs.writeFileSync(
+                `./src/data/${filename}.json`,
+                JSON.stringify(mainExplore),
+              );
+            }
+          };
 
           await tryFetch();
+
+          for (const move of history) {
+            // Fill in missing spots lazily (as blanks)
+            if (!explore.m[move]) {
+              explore.m[move] = {
+                d: [0, 0, 0],
+              };
+            }
+
+            explore = explore.m[move];
+            appliedMoves.push(move);
+            appliedBoard.move(move);
+
+            await tryFetch();
+          }
         }
       }
+    };
+
+    const combineInto = (smallFile: string, deepFile: string) => {
+      console.log(`combining ${smallFile} into ${deepFile}`);
+
+      const smallExplore: CompactLichessExplore = JSON.parse(
+        fs.readFileSync(`./src/data/${smallFile}.json`, "utf8"),
+      );
+
+      const deepExplore: CompactLichessExplore = JSON.parse(
+        fs.readFileSync(`./src/data/${deepFile}.json`, "utf8"),
+      );
+
+      const combinedExplore = combineCompactLichessExplore(
+        smallExplore,
+        deepExplore,
+      );
+
+      fs.writeFileSync(
+        `./src/data/${deepFile}.json`,
+        JSON.stringify(combinedExplore),
+      );
+    };
+
+    await scan("lichessExploreBlitzLow", "blitzLow", false);
+    await scan("lichessExploreMasters", "masters", false);
+    await scan("lichessExploreBlitzHigh", "blitzHigh", false);
+    await scan("lichessExploreRapidLow", "rapidLow", false);
+    await scan("lichessExploreRapidHigh", "rapidHigh", false);
+
+    combineInto("lichessExploreBlitzLow", "lichessExploreBlitzLowDeep");
+    combineInto("lichessExploreMasters", "lichessExploreMastersDeep");
+    combineInto("lichessExploreBlitzHigh", "lichessExploreBlitzHighDeep");
+    combineInto("lichessExploreRapidLow", "lichessExploreRapidLowDeep");
+    combineInto("lichessExploreRapidHigh", "lichessExploreRapidHighDeep");
+
+    await scan("lichessExploreBlitzLowDeep", "blitzLow", true);
+    await scan("lichessExploreMastersDeep", "masters", true);
+    await scan("lichessExploreBlitzHighDeep", "blitzHigh", true);
+    await scan("lichessExploreRapidLowDeep", "rapidLow", true);
+    await scan("lichessExploreRapidHighDeep", "rapidHigh", true);
+  };
+
+  let failed = true;
+  while (failed) {
+    try {
+      await main();
+
+      failed = false;
+    } catch (e) {
+      console.error(e);
+
+      console.log("3min timeout");
+
+      await sleep(1000 * 60 * 3); // 3 min cooldown
     }
-  };
-
-  const combineInto = (smallFile: string, deepFile: string) => {
-    console.log(`combining ${smallFile} into ${deepFile}`);
-
-    const smallExplore: CompactLichessExplore = JSON.parse(
-      fs.readFileSync(`./src/data/${smallFile}.json`, "utf8"),
-    );
-
-    const deepExplore: CompactLichessExplore = JSON.parse(
-      fs.readFileSync(`./src/data/${deepFile}.json`, "utf8"),
-    );
-
-    const combinedExplore = combineCompactLichessExplore(
-      smallExplore,
-      deepExplore,
-    );
-
-    fs.writeFileSync(
-      `./src/data/${deepFile}.json`,
-      JSON.stringify(combinedExplore),
-    );
-  };
-
-  await scan("lichessExploreBlitzLow", "blitzLow", false);
-  await scan("lichessExploreMasters", "masters", false);
-  await scan("lichessExploreBlitzHigh", "blitzHigh", false);
-  await scan("lichessExploreRapidLow", "rapidLow", false);
-  await scan("lichessExploreRapidHigh", "rapidHigh", false);
-
-  combineInto("lichessExploreBlitzLow", "lichessExploreBlitzLowDeep");
-  combineInto("lichessExploreMasters", "lichessExploreMastersDeep");
-  combineInto("lichessExploreBlitzHigh", "lichessExploreBlitzHighDeep");
-  combineInto("lichessExploreRapidLow", "lichessExploreRapidLowDeep");
-  combineInto("lichessExploreRapidHigh", "lichessExploreRapidHighDeep");
-
-  await scan("lichessExploreBlitzLowDeep", "blitzLow", true);
-  await scan("lichessExploreMastersDeep", "masters", true);
-  await scan("lichessExploreBlitzHighDeep", "blitzHigh", true);
-  await scan("lichessExploreRapidLowDeep", "rapidLow", true);
-  await scan("lichessExploreRapidHighDeep", "rapidHigh", true);
+  }
 })();
