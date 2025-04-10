b281cf06dd1bea7337418273342b35a51523611c
Jonathan Olson <jonathan.olson@colorado.edu>
2025-04-10 07:29:56 -0600

    data

--- scripts/boostLines.ts
+++ scripts/boostLines.ts
@@ -1,5 +1,9 @@
+import { Move } from "../src/model/common.js";
+
 export const boostLines: Move[][] = [
-  // ["Nf3"],
+  ["Nf3"],
+  ["Nf3", "d5", "d4", "Nf6", "Nc3"],
+  // ["Nf3", "d5", "d4", "Nf6", "Nc3", "c5", "Bf4"],
   // ["Nf3", "d5"],
   // ["e4", "e6", "d3", "d5", "Nd2"],
   // // ["d4", "d5", "Bf4", "c5"],
