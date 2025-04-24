587f92adf59b1a7ba7ee7312f0ac578b8ebeb424
Jonathan Olson <jonathan.olson@colorado.edu>
2025-04-24 11:14:28 -0600

    win stats bar refactoring

--- src/view/WinStatisticsBar.ts
+++ src/view/WinStatisticsBar.ts
@@ -1,111 +1,143 @@
-import {
-  Line,
-  Node,
-  NodeOptions,
-  Path,
-  Rectangle,
-  TPaint,
-} from "scenerystack/scenery";
+import { Line, Node, NodeOptions, Path, Rectangle } from "scenerystack/scenery";
 import { LichessExploreWins } from "../model/getLichessExplore";
 import { Bounds2 } from "scenerystack/dot";
-import { combineOptions } from "scenerystack/phet-core";
 import { Shape } from "scenerystack/kite";
 
 export type WinStatisticsBarOptions = NodeOptions;
 
+const stroke = "#888";
+const whiteFill = "#fff";
+const drawFill = "#888";
+const blackFill = "#000";
+const barHeight = 15;
+
+const triangleExtent = 5;
+
+const triangleShape = new Shape()
+  .moveTo(-triangleExtent, 0)
+  .lineTo(triangleExtent, 0)
+  .lineTo(0, triangleExtent)
+  .close()
+  .makeImmutable();
+
 export class WinStatisticsBar extends Node {
+  private leftRectangle: Rectangle = new Rectangle({
+    stroke: stroke,
+  });
+  private drawRectangle: Rectangle = new Rectangle({
+    stroke: stroke,
+    fill: drawFill,
+  });
+  private rightRectangle: Rectangle = new Rectangle({
+    stroke: stroke,
+  });
+  private winPercentageLine: Line = new Line({
+    y2: barHeight * 0.75,
+    stroke: "#f00",
+    lineCap: "butt",
+  });
+  private winPercentageTriangle: Path = new Path(triangleShape, {
+    fill: "#f00",
+    x: triangleExtent, // just an initial value that puts us within the basic bounds
+  });
+
   public constructor(
     lichessWins: LichessExploreWins | null,
     whiteOnLeft: boolean,
     winPercentage: number | null,
     providedOptions?: WinStatisticsBarOptions,
   ) {
+    super(providedOptions);
+
+    this.children = [
+      this.leftRectangle,
+      this.drawRectangle,
+      this.rightRectangle,
+      this.winPercentageLine,
+      this.winPercentageTriangle,
+    ];
+
+    this.setData(lichessWins, whiteOnLeft, winPercentage);
+  }
+
+  public setData(
+    lichessWins: LichessExploreWins | null,
+    whiteOnLeft: boolean,
+    winPercentage: number | null,
+  ): void {
     const barWidth = 150;
-    const barHeight = 15;
 
-    const bar = new Rectangle(0, 0, barWidth, barHeight, {
-      layoutOptions: { column: 1, row: 0 },
-    });
-    if (lichessWins) {
-      const stroke = "#888";
-      const whiteFill = "#fff";
-      const drawFill = "#888";
-      const blackFill = "#000";
+    const totalResults = lichessWins
+      ? lichessWins.whiteWins + lichessWins.blackWins + lichessWins.draws
+      : 0;
+    const hasResults = totalResults > 0;
 
-      const total =
-        lichessWins.whiteWins + lichessWins.blackWins + lichessWins.draws;
-      const toX = (count: number) => (barWidth * count) / total;
+    this.leftRectangle.visible = hasResults;
+    this.drawRectangle.visible = hasResults;
+    this.rightRectangle.visible = hasResults;
 
-      const addRectangle = (n0: number, n1: number, fill: TPaint) => {
-        if (n0 !== n1) {
-          bar.addChild(
-            Rectangle.bounds(new Bounds2(toX(n0), 0, toX(n1), barHeight), {
-              fill: fill,
-              stroke: stroke,
-            }),
-          );
-        }
-      };
+    // TODO: proper positioning if no results
 
-      if (whiteOnLeft) {
-        addRectangle(0, lichessWins.whiteWins, whiteFill);
-        addRectangle(
-          lichessWins.whiteWins,
-          lichessWins.whiteWins + lichessWins.draws,
-          drawFill,
-        );
-        addRectangle(
-          lichessWins.whiteWins + lichessWins.draws,
-          total,
-          blackFill,
-        );
+    const farLeft = 0;
+    let drawLeft = 0;
+    let drawRight = barWidth;
+    const farRight = barWidth;
+
+    if (hasResults && lichessWins) {
+      this.leftRectangle.fill = whiteOnLeft ? whiteFill : blackFill;
+      this.rightRectangle.fill = whiteOnLeft ? blackFill : whiteFill;
+
+      const toX = (count: number) => (barWidth * count) / totalResults;
+
+      const leftWins = whiteOnLeft
+        ? lichessWins.whiteWins
+        : lichessWins.blackWins;
+      drawLeft = toX(leftWins);
+      drawRight = toX(leftWins + lichessWins.draws);
+    }
+    this.leftRectangle.rectBounds = new Bounds2(
+      farLeft,
+      0,
+      drawLeft,
+      barHeight,
+    );
+    this.drawRectangle.rectBounds = new Bounds2(
+      drawLeft,
+      0,
+      drawRight,
+      barHeight,
+    );
+    this.rightRectangle.rectBounds = new Bounds2(
+      drawRight,
+      0,
+      farRight,
+      barHeight,
+    );
+
+    const hasWinPercentage = winPercentage !== null;
+    this.winPercentageLine.visible = hasWinPercentage;
+    this.winPercentageTriangle.visible = hasWinPercentage;
+
+    if (hasWinPercentage) {
+      // Apply padding of 0.5 so we don't go OVER things
+      const winPercentageX = 0.5 + ((barWidth - 1) * winPercentage) / 100;
+
+      this.winPercentageLine.setLine(
+        winPercentageX,
+        0,
+        winPercentageX,
+        barHeight * 0.75,
+      );
+
+      if (
+        winPercentageX > triangleExtent &&
+        winPercentage < barWidth - triangleExtent
+      ) {
+        this.winPercentageTriangle.x = winPercentageX;
       } else {
-        addRectangle(0, lichessWins.blackWins, blackFill);
-        addRectangle(
-          lichessWins.blackWins,
-          lichessWins.blackWins + lichessWins.draws,
-          drawFill,
-        );
-        addRectangle(
-          lichessWins.blackWins + lichessWins.draws,
-          total,
-          whiteFill,
-        );
-      }
-
-      if (winPercentage !== null) {
-        // Apply padding of 0.5 so we don't go OVER things
-        const winPercentageX = 0.5 + ((barWidth - 1) * winPercentage) / 100;
-        bar.addChild(
-          new Line(winPercentageX, 0, winPercentageX, barHeight * 0.75, {
-            stroke: "#f00",
-            lineCap: "butt",
-          }),
-        );
-
-        if (winPercentageX > 5 && winPercentage < barWidth - 5) {
-          const triangleShape = new Shape()
-            .moveTo(-5, 0)
-            .lineTo(5, 0)
-            .lineTo(0, 5)
-            .close();
-          bar.addChild(
-            new Path(triangleShape, {
-              fill: "#f00",
-              x: winPercentageX,
-            }),
-          );
-        }
+        this.winPercentageTriangle.x = triangleExtent;
+        this.winPercentageTriangle.visible = false;
       }
     }
-
-    super(
-      combineOptions<NodeOptions>(
-        {
-          children: [bar],
-        },
-        providedOptions,
-      ),
-    );
   }
 }
